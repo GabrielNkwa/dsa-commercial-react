0b4f451caf2c487ffcc8c236067bb1b683d6b79e
Gabriel Nkwa <gnkwa@outlook.com>
2024-02-13 22:41:05 +0100

    newspage

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,18 +1,26 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Navbar from '../Layout/Navbar';
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
+  const error = useRouteError();
+  console.error(error);
 
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center j-screen">
+        <h1 className="uppercase font-bold text-4xl tracking-wide mb-5 md:text-6xl  ">
+          Error 404 | Page Not Found
+        </h1>
 
-  return(
-    <div>
-        <h1>An error occurred</h1>
-        <p>Sorry, an unexpected error has occured.</p>
-        <p>
-            <i>{error.statusText || error.message}</i>
-        </p>
-    </div>
-  )
-   ;
+        <Link
+          to="/"
+          className="py-2 py-2 px-6 rounded shadow text-white bg-black hover:bg-transparent border-2 border-black transition-all duration-500 hover:text-black font-bold mb-5"
+        >
+          Back to Home
+        </Link>
+      </div>
+    </>
+  );
 }
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -3,6 +3,7 @@ import Navbar from '../Layout/Navbar';
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import client from '../client';
+import SanityBlockContent from '@sanity/block-content-to-react';
 
 export default function SinglePost() {
   const [singlePost, setSinglePost] = useState([]);
@@ -36,27 +37,38 @@ export default function SinglePost() {
     <>
       <Navbar />
       {isloading ? (
-        <h1>Loading...</h1>
+        <h1 className="uppercase font-bold text-4xl tracking-wide mb-5 md:text-6xl lg:text-8xl flex items-center justify-center h-screen">
+          Loading...
+        </h1>
       ) : (
         <>
-          <section className='px-5'>
-            <h1 className=''>{singlePost.title}</h1>
+          <section className="px-5 xl:max-w-6xl xl:mx-auto pb-10">
+            <h1 className="uppercase font-bold text-4xl tracking-wide mb-5 md:text-6xl lg:text-8xl text-center mt-5 ">
+              {singlePost.title}
+            </h1>
             {singlePost.mainImage && singlePost.mainImage.asset && (
               <img
                 src={singlePost.mainImage.asset.url}
                 alt={singlePost.title}
                 title={singlePost.title}
+                className="blog__image rounded-top"
               />
             )}
-
-            <button>
-              <Link
-                to="/news"
-                className="py-2 py-2 px-6 rounded shadow text-white bg-black hover:bg-transparent border-2 border-black transition-all duration-500 hover:text-black font-bold mb-5"
-              >
-                Read more news stories
-              </Link>
-            </button>
+            <div className="block__content">
+              <SanityBlockContent
+                blocks={singlePost.body}
+                projectId="ltajmjiu"
+                dataset="production"
+              />
+              <button>
+                <Link
+                  to="/news"
+                  className="py-2 py-2 px-6 rounded shadow text-white bg-black hover:bg-transparent border-2 border-black transition-all duration-500 hover:text-black font-bold mb-5"
+                >
+                  Read more news stories
+                </Link>
+              </button>
+            </div>
           </section>
         </>
       )}
